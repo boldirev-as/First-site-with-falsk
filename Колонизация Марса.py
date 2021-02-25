c3985b35e9bd01224ff553585946cc37e4a0da28
boldirev-as <60057518+boldirev-as@users.noreply.github.com>
2021-02-25 19:14:56 +0500

    e

--- Колонизация Марса.py
+++ Колонизация Марса.py
@@ -224,5 +224,34 @@ def choice_planet_name(planet_name):
                 </html>"""
 
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                    <title>Результаты</title>
+                  </head>
+                  <body>
+                    <h1>Результаты отбора</h1>
+                    <h2>Претенденты на участии в миссии: {nickname}</h2>
+                    <div class="alert alert-warning" role="alert">
+                      Поздравляем! Ваш рейтинг после {level} этапа отбора
+                    </div>
+                    <div class="alert alert-info" role="alert">
+                      составляет {rating}!
+                    </div>
+                    <div class="alert alert-light" role="alert">
+                      Желаем удчаи!
+                    </div>
+                  </body>
+                </html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
