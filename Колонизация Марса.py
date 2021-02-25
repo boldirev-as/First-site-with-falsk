6580749910f24047f395a87b1af9878c075683e0
boldirev-as <60057518+boldirev-as@users.noreply.github.com>
2021-02-25 18:13:50 +0500

    b

--- Колонизация Марса.py
+++ Колонизация Марса.py
@@ -41,6 +41,10 @@ def image_mars():
 
 @app.route('/promotion_image')
 def promotion_image():
+    countdown_list = ["</br>Человечество вырастает из детства.", "Человечеству мала одна планета.",
+                      "Мы сделаем обитаемыми безжизненные пока планеты.", "И начнем с Марса!",
+                      "Присоединяйся!", 'Пуск!']
+
     return f"""<!doctype html>
                 <html lang="en">
                   <head>
@@ -50,13 +54,32 @@ def promotion_image():
                         href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
                         integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
                         crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
 
                     <title>Привет, Марс!</title>
                   </head>
                   <body>
                     <h1>Жди нас, Марс!</h1>
                     <img src="{url_for('static', filename='img/mars.png')}" 
-                        alt="здесь должна была быть картинка, но не нашлась">
+                        alt="здесь должна была быть картинка, но не нашлась"></br>
+                    <div class="alert alert-success" role="alert">
+                      {countdown_list[0]}
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                      {countdown_list[1]}
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                      {countdown_list[2]}
+                    </div>
+                    <div class="alert alert-info" role="alert">
+                      {countdown_list[3]}
+                    </div>
+                    <div class="alert alert-light" role="alert">
+                      {countdown_list[4]}
+                    </div>
+                    <div class="alert alert-dark" role="alert">
+                      {countdown_list[5]}
+                    </div>
                   </body>
                 </html>"""
 
