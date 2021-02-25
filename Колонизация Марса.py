36c3615ac4991941f97b43a433af9f906b5ddbc8
boldirev-as <60057518+boldirev-as@users.noreply.github.com>
2021-02-25 19:02:59 +0500

    d

--- Колонизация Марса.py
+++ Колонизация Марса.py
@@ -186,5 +186,43 @@ def form_sample():
         return "Форма отправлена"
 
 
+@app.route('/choice/<planet_name>')
+def choice_planet_name(planet_name):
+    countdown_list = ["Эта планета близка к Земле", "На ней много необходимых ресурсов",
+                      "На ней есть вода и атмосфера", "На ней есть небольшое магнитное поле",
+                      "Наконец, она просто красива!"]
+
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                    <title>Варианты выбора</title>
+                  </head>
+                  <body>
+                    <h1>Моё предложение: {planet_name}</h1>
+                    <div class="alert alert-success" role="alert">
+                      {countdown_list[0]}
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                      {countdown_list[1]}
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                      {countdown_list[2]}
+                    </div>
+                    <div class="alert alert-info" role="alert">
+                      {countdown_list[3]}
+                    </div>
+                    <div class="alert alert-light" role="alert">
+                      {countdown_list[4]}
+                    </div>
+                  </body>
+                </html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
