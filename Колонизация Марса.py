2649e9a999d415e9684c6757defd948e3e81c6ff
boldirev-as <60057518+boldirev-as@users.noreply.github.com>
2021-02-26 10:19:48 +0500

    g

--- Колонизация Марса.py
+++ Колонизация Марса.py
@@ -322,5 +322,48 @@ document.getElementById('files').addEventListener('change', handleFileSelect, fa
         return "Форма отправлена"
 
 
+@app.route('/carousel')
+def carousel():
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                    <title>Марс</title>
+                  </head>
+                  <body>
+                    <h1>Пейзажи Марса</h1>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" 
+                        integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" 
+                        crossorigin="anonymous"></script>
+                    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                      <div class="carousel-inner">
+                        <div class="carousel-item active">
+                          <img src="{url_for('static', filename='img/mars.png')}" class="d-block w-100" alt="1">
+                        </div>
+                        <div class="carousel-item">
+                          <img src="{url_for('static', filename='img/mars1.png')}" class="d-block w-100" alt="2">
+                        </div>
+                        <div class="carousel-item">
+                          <img src="{url_for('static', filename='img/mars2.png')}" class="d-block w-100" alt="3">
+                        </div>
+                      </div>
+                      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                      </button>
+                      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                      </button>
+                    </div>
+                  </body>
+                </html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
